3e124057b16eeaa1e363b882e3afa16ba4557730
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-14 15:00:12 +0200

    [ADD] List logic

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const listaSpesa = ["uova", "latte", "pasta", "frutta", "verdura", "carne"];
 
 let indice = 0;
+const ul = document.querySelector("ul");
+const li = document.querySelector("li");
+
 
 while(indice < listaSpesa.length){
-  const ul = document.querySelector("ul");
-  const li = document.querySelector("li");
   const prodotti = listaSpesa[indice];
-
   ul.append(li)
   li.append(prodotti)
 
